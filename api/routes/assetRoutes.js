12ed8a3b237e53f3934d437fca08ba10edf54168
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-04-19 17:16:22 +0000

    bug

--- api/routes/assetRoutes.js
+++ api/routes/assetRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 const assetController = require('../controllers/assetController');
 
-router.get('/ByClass/:classId/:inicio/:termino', assetController.getAssetsByClassWithResult);
+router.get('/ByClass/:classId/:inicio/:termino', authMiddleware, assetController.getAssetsByClassWithResult);
 router.get('/Rendimentos/:assetId/:classId/:inicio/:termino', authMiddleware, assetController.getRendimentos);
 
 module.exports = router;
