d9aba5805dfcb1d096e6a6e3bf0a649976b19ea3
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-04-24 18:14:47 -0300

    Add POST route for PDF upload
    Added POST route for PDF upload with multer middleware.

--- api/routes/transactionRoutes.js
+++ api/routes/transactionRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transactionController');
 const authMiddleware = require('../middlewares/auth');
+const multer = require('multer');
 
-router.get('/addPDF', authMiddleware, transactionController.addPDF);
+// Configuração do upload aqui nas rotas
+const upload = multer({ dest: '/tmp/' });
+
+// Rota POST com o middleware upload.single
+router.post('/addPDF', authMiddleware, upload.single('nota'), transactionController.addPDF);
 
 module.exports = router;
