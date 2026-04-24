6ea44cc8dcd125d0d4d93b5671e0793dccebd536
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-04-24 17:34:45 -0300

    Add transaction routes with CRUD operations

--- api/routes/transactionRoutes.js
+++ api/routes/transactionRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const classController = require('../controllers/classController');
+const authMiddleware = require('../middlewares/auth');
 
+router.get('/', authMiddleware, classController.getAll);
+router.post('/', classController.create);
+router.delete('/:id', classController.delete);
+router.put('/:id', classController.update);
+
+module.exports = router;
