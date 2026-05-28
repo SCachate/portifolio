da7ba1104c7982a0732164f0e7a69f55ff605947
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-05-28 15:06:06 -0300

    Update incomeRoutes.js

--- api/routes/incomeRoutes.js
+++ api/routes/incomeRoutes.js
@@ -1,1 +1,5 @@
-
+const express = require('express');
+const router = express.Router();
+const incomeController = require('../controllers/incomeController');
+router.get('/dividendosB3', incomeController.dividendosB3);
+module.exports = router;
