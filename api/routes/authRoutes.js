31717e715f8ed77a0da03186de577d6b2b615f35
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-06-02 18:00:56 +0000

    acerto

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Xisto
-
 router.post('/google', authController.googleLogin);
 
 module.exports = router;
