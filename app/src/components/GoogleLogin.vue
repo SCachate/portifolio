1c4fe58b495c54b265fe48ae9a6d8d9cd07cfcd5
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-06-30 17:02:44 +0000

    teste

--- app/src/components/GoogleLogin.vue
+++ app/src/components/GoogleLogin.vue
@@ -1,37 +1,3 @@
-<!--
-<template>
-  <div ref="googleButtonRef" class="w-full flex justify-center"></div>
-</template>
-
-<script setup>
-import { onMounted, ref } from 'vue';
-
-const emit = defineEmits(['success']);
-const googleButtonRef = ref(null);
-const Client_Id = "289336503031-s8athopc2o6dsqk8f79dln4318l7b3o2.apps.googleusercontent.com";
-const loading = ref(true);
-
-const initGoogle = () => {
-  if (window.google) {
-    window.google.accounts.id.initialize({
-      client_id: Client_Id,
-      callback: (response) => emit('success', response),
-      auto_select: false,
-    });
-    
-    window.google.accounts.id.renderButton(
-      googleButtonRef.value, // Referência direta ao elemento
-      { theme: "outline", size: "large", type: "standard", shape: "pill" }
-    );
-  } else {
-    setTimeout(initGoogle, 500);
-  }
-};
-
-onMounted(initGoogle);
-</script>
-
--->
 <template>
   <button @click="loginComGoogle" class="flex items-center justify-center gap-2 px-4 py-2 border border-gray-300 rounded-full shadow-sm bg-white hover:bg-gray-50 transition-colors w-full font-medium text-gray-700">
     <svg class="w-5 h-5" viewBox="0 0 24 24">
