ecaa160b6b17fe9f6d625f1fbf06c1d6427489d2
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-08-29 17:58:29 +0000

    teste

--- app/src/components/Resumo.vue
+++ app/src/components/Resumo.vue
@@ -429,7 +429,7 @@ const historicoResultadoOptions = computed(() => ({
               style: 'currency',
               currency: 'BRL',
               minimumFractionDigits: 0,
-              maximumFractionDigits: 0
+              maximumFractionDigits: 2
             });
           }
         }
