15f019ed5fd622b1cbd9248f74ae9765fc2267c8
Sidnei Cachate <76235515+SCachate@users.noreply.github.com>
2026-08-29 15:20:24 +0000

    teste

--- app/src/components/Resumo.vue
+++ app/src/components/Resumo.vue
@@ -226,7 +226,7 @@ const atualizarTudo = async () => {
 const sharedGridOptions = {
   borderColor: '#334155',
   strokeDashArray: 4,
-  xaxis: { lines: { show: false, color: 'transparent' } }, // <--- REMOVE LINHAS VERTICAIS
+  xaxis: { lines: { show: false, strokeWidth: 0, opacity: 0 } }, // <--- REMOVE LINHAS VERTICAIS
   yaxis: { lines: { show: true } }  // <--- MANTÉM LINHAS HORIZONTAIS
 };
 
